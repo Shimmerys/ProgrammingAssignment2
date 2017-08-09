abe161443714cae70a13b74809601c3885c66137
Susan <susansoojeongshim@gmail.com>
2017-08-09 14:44:17 +0900

    Assignment cacheMatrix.R upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix calculates the inverse of a matrix via the solve() function.
+## cacheSolve checks if the inverse of the matrix has already been calculated.
 
-## Write a short comment describing this function
+## makeCacheMatrix receives a matrix as input and returns its inverse as its output.
 
 makeCacheMatrix <- function(x = matrix()) {
 invers <- NULL
@@ -16,7 +16,8 @@ list(set=set, get=get, setinvers=setinvers, getinvers=getinvers)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve receives a matrix as input and returns the inverse of the matrix IF it has not been calculated.
+## If the inverse of the matrix has already been calculated, it skips the calculation.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
